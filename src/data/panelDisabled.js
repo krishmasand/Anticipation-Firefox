6b11de6cd9005edd15efa940c512a2111b9332dc
Krish Masand <krishmasand@gmail.com>
2015-07-28 21:18:44 -0500

    Got rid of jquery

--- src/data/panelDisabled.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-      $('#message').html("Anticipation is disabled, YouTube time will be shown. Refresh your tab if you are already on YouTube.");
-})
-
--- src/data/panelEnabled.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-      $('#message').html("Anticipation is enabled, YouTube time will be hidden. Refresh your tab if you are already on YouTube.");
-})
-
